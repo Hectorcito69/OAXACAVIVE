ceb8ae15e7055d11491040e76c4608c2965f03d1
Hectorcito69 <jimenezjimenezhectorsebastian@gmail.com>
2026-07-07 20:56:43 -0600

    actualizacion de la imagen de la pagina de inicio

--- src/data/eventos.js
+++ src/data/eventos.js
@@ -10,7 +10,7 @@ export const eventos = [
     titulo: "OAXACA VIVE — Guelaguetza", // Puedes cambiar este título
     lugar: "Lugar del evento", // Puedes cambiar este lugar
     fechas: "Julio 16",
-    flyer: "/images/OAXACAVIVEFLYER-ACTUAL.jpg",
+    flyer: "/images/OAXACA VIVE FLYER-3.png",
     descripcion: `Donde la esencia de Oaxaca cobra vida. Déjate envolver por la magia, el color y la riqueza cultural de nuestra tierra en Oaxaca Vive, un espectáculo único concebido como un mosaico multidisciplinario que reúne las expresiones artísticas más vibrantes de la región.
 
 Una experiencia inolvidable que te hará vibrar y sentir el verdadero corazón de Oaxaca. ¡Asegura tu lugar desde ahora! Haz clic en el botón de abajo o escanea el código QR de nuestra publicidad para comprar tus boletos hoy mismo.`,
